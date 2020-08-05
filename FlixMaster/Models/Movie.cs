9eaf06c9e843435cf34a66bf4f3b1b314f90ed5e
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-05 15:28:33 -0700

    Add models for Showing and Movie.
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored-by: TeresaRosinski <trosinski89@gmail.com>

--- FlixMaster/Models/Movie.cs
+++ FlixMaster/Models/Movie.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
+using System;
+
 namespace FlixMaster.Models
 {
   public class Movie
   {
+    public Movie ()
+    {
+      this.Showings = new HashSet<MovieShowing>();
+    }
     
+    public int MovieId {get; set; }
+    public string Title {get; set;}
+    public string Description {get; set; }
+    public string Genre {get; set;}
+    public string Rating {get; set; }
+
+    public List<string> Actors = new List<string> (); 
+
+    public virtual ICollection<MovieShowing> Showings {get;}
+
   }
 }
--- FlixMaster/Models/Showing.cs
+++ FlixMaster/Models/Showing.cs
@@ -1,7 +1,21 @@
+using System.Collections.Generic;
+using System;
+using System.ComponentModel;
+
 namespace FlixMaster.Models
 {
   public class Showing
   {
-    
+    public Showing() {
+      this.Movies = new HashSet<MovieShowing>();
+    }
+    public int ShowingId { get; set; }
+
+    [DisplayName("Date:")]
+    public DateTime ShowingDate { get; set; }
+
+    [DisplayName("Time:")]
+    public DateTime ShowingTime { get; set; }
+    public virtual ICollection<MovieShowing> Movies { get; set; }
   }
 }
