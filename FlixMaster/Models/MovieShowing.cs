0699efaf6303f69a315226ce75316fc492f383af
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-05 15:39:32 -0700

    Add MovieShowing model.
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored-by: TeresaRosinski <trosinski89@gmail.com>

--- FlixMaster/Models/MovieShowing.cs
+++ FlixMaster/Models/MovieShowing.cs
@@ -3,7 +3,11 @@ namespace FlixMaster.Models
 {
   public class MovieShowing
   {
-    
+    public int MovieShowingId { get; set;}
+    public int MovieId { get; set; }
+    public Movie Movie { get; set; }
+    public int ShowingId { get; set; }
+    public Showing Showing { get; set; }
   }
 }
 
