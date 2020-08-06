341374241be86743829367aecb1edaf13463756c
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-06 13:45:09 -0700

    Add form to create new movie showing, add backslash to buttons, update Showing controller.
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored-by: TeresaRosinski <trosinski89@gmail.com>

---
+++ FlixMaster/Controllers/ShowingsController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using FlixMaster.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FlixMaster.Controllers
+{
+  public class ShowingsController : Controller
+  {
+    private readonly FlixMasterContext _db;
+
+    public ShowingsController(FlixMasterContext db)
+    {
+    _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Showings.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.Title = new SelectList (_db.Movies, "MovieId", "Title"); //MovieID associates with specific instance of movie - title associates with that specific field
+      ViewBag.Rating = new SelectList (_db.Movies, "MovieId", "Rating");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Showing showing)
+    {
+      _db.Showings.Add(showing);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
